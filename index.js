e19d1879ef43b59adb7482dc7f5edb90db3f61fc
Oliver Vahenõmm <oliver-v@live.com>
2025-12-27 12:01:40 +0200

    chore: update message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require("@actions/core");
 
 try {
     const message = core.getInput("message");
-    console.log(`Message: ${message}`);
+    console.log(`Message from GH action: ${message}`);
 } catch (error) {
     core.setFailed(error.message);
 }
